e77403f26c341a156d68cb70d86f1f2bf0f9c46c
jn_xyp <jinan.xyp@gmail.com>
2025-04-16 02:02:56 -0400

    feat(orm): add more data types

--- src/models.py
+++ src/models.py
@@ -9,19 +9,35 @@ class ModInfoType(Enum):
     REPOSTED = 2
 
 
+class ThreadMeta(SQLModel):
+    tid: int
+    uid: int
+    featured: int  # 是否精华帖 0 否 1-3 精华1-3
+    recommend_weight: int  # 推荐数
+
+
+class AdminNotes(SQLModel):
+    mod_index_comment: str | None = None
+    thread_comment: str | None = None  # db field name: adminThreadComment
+
+
 class ModInfo(SQLModel):
     mod_info_type: ModInfoType
     mod_id: str
     mod_name_cn: str
     mod_author: str
     mod_category: str  # todo: Enum from DB
-    mod_version: str
+    mod_game_version: str  # todo: Enum from DB, db field name: modVersion
+    mod_version: str  # db field name: modReleaseVersion
     mod_safe_rm: bool
     mod_dependencies: List[str] = []
     mod_conflicts: List[str] = []
     mod_short_desc: str
     mod_language: str  # todo: Enum from DB
     mod_update_date: str  # todo: DateTime from DB
+
+    admin_notes: AdminNotes | None = None
+    thread_meta: ThreadMeta | None = None
 
 
 class ModInfoOriginal(ModInfo):
@@ -32,11 +48,11 @@ class ModInfoOriginal(ModInfo):
 class ModInfoTranslated(ModInfo):
     mod_info_type: ModInfoType = ModInfoType.TRANSLATED
     mod_name_en: str
-    mod_publish_site: str
+    mod_publish_url: str  # db field name: modPublishSite
     mod_translator: str
 
 
 class ModInfoReposted(ModInfo):
     mod_info_type: ModInfoType = ModInfoType.REPOSTED
     mod_name_en: str
-    mod_publish_site: str
+    mod_publish_url: str  # db field name: modPublishSite
